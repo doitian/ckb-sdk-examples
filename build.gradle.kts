108ce7af7699d6a4e6478c66c9ae35a3b32590ca
ian <me@iany.me>
2023-07-27 10:57:17 +0800

    junit hello world

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,24 @@ plugins {
     java
 }
 
+sourceSets {
+    main {
+        java {
+            setSrcDirs(listOf("env"))
+        }
+    }
+
+    test {
+        java {
+            setSrcDirs(listOf("examples"))
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
 dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.7.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
---
+++ examples/transfer-ckb/TransferCKBTest.java
@@ -0,0 +1,10 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+import env.Env;
+
+class TransferCKBTest {
+    @Test
+    void addition() {
+        assertEquals(2, Env.add(1, 1));
+    }
+}
